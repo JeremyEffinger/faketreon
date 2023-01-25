9f88aa9efcdcef863b4e929b74dc2f8d20a8b91a
JeremyEffinger <Jeremy.Effinger@gmail.com>
2023-01-25 15:38:18 -0600

    recoil, dotenv, folder structure changes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./styling/App.css";
 import axios from "axios";
 import { useState } from "react";
 
--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
-import express, { json } from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import postgress from "postgres";
+
+//setup server
 const PORT = 6000;
 const server = express();
 
+//setup database
+const db_url = process.env.FAKETREON_DB_URL;
+const sql = postgress(db_url);
+
+//start middleware
 server.use(express.json());
 
+//api routes
 server.get("/api/v1", (req, res) => {
   res.json({ message: "Hello!" });
 });
 
+//fail over routes
 server.use((req, res, next) => {
   res.contentType("text/plain").status(404).send("Not Found");
 });
@@ -16,5 +28,6 @@ server.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send("Internal Server Error");
 });
-
+console.log(sql);
+//start server
 server.listen(PORT, () => console.log(`start listening on port : ${PORT}`));
