317c411f34f7cdba009afa53e76fbb40d8fa35a0
Alex Starr <116136090+HighBornStarr@users.noreply.github.com>
2023-01-26 15:37:43 -0600

    Membership & css

--- client/src/components/membership/Card/Card.jsx
+++ client/src/components/membership/Card/Card.jsx
@@ -1,10 +1,13 @@
+import './card.css';
+
+
 export const Card = (props) => {
 
   return (
     <div className='card'>
       <div className='card__body'>
-      <img className="photo" src={props.dummyimage} />
-        <h2 className='card__title'></h2>
+      <img className="photo" src={props.dummyimage}class="card__image" />
+        <h2 className='card__title'>Warlock Aethereal and Strange</h2>
         <p className='card__description'>This is the filler text for what will be warlock information and bullet points.</p>
       </div>
       <button className='card__btn'>Join</button>
