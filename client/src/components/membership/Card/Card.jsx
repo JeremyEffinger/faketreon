b9564471586b0817b7b50e3397f1234da545a06b
Alex Starr <116136090+HighBornStarr@users.noreply.github.com>
2023-01-27 10:25:32 -0600

    add btns /etc

--- client/src/components/membership/Card/Card.jsx
+++ client/src/components/membership/Card/Card.jsx
@@ -6,7 +6,7 @@ export const Card = (props) => {
   return (
     <div className='card'>
       <div className='card__body'>
-      <img className="photo" src={props.dummyimage} />
+      <img className="card__image" src={props.dummyimage} />
         <h2 className='card__title'>Warlock Aethereal and Strange</h2>
         <p className='card__description'>This is the filler text for what will be warlock information and bullet points.</p>
       </div>
