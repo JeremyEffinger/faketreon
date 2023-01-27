07c40f32e67d3431a017a2a21706380529f4746f
cristian-kittsteiner <cdkittsteiner@yahoo.com>
2023-01-26 19:33:27 -0600

    updates to aboutMe

---
+++ client/src/components/about-me/patronBoons/PatronBoons.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function PatronBoons(){
+
+    return (
+        <div>
+            <div className="patronBoons">
+                    <h4>
+                        become a patron
+                    </h4>
+                    <span className="images">
+                        <div className="pix">
+                        image 1
+                        </div>
+                        <div className="pix">
+                        image 2   
+                        </div>
+                        <div className="pix">
+                        image 3    
+                        </div>
+                        <div className="pix">
+                        image 4   
+                        </div>
+                    </span>
+                </div>
+        </div>
+    )
+}
+
+export default PatronBoons
