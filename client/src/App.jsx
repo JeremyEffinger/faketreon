c76f88c39f1d77ee7231408e4321b79d0c2adf59
JeremyEffinger <Jeremy.Effinger@gmail.com>
2023-02-01 11:09:12 -0600

    React router functional.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import "./styling/App.css";
 import axios from "axios";
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Campaign from "./containers/Campaign/Campaign.jsx";
@@ -19,15 +20,19 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Header />
-      <Campaign />
-      <div>
-        <button onClick={getDataFromServer}>Access server using proxy</button>
-        <p>data : {data.message}</p>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="*" element={<Campaign />} />
+        </Routes>
+        <Footer />
+        <div>
+          <button onClick={getDataFromServer}>Access server using proxy</button>
+          <p>data : {data.message}</p>
+        </div>
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
