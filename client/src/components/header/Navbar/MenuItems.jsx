8ceeeb681e9f684696676c540069836025835fde
blancobread <115666624+blancobread@users.noreply.github.com>
2023-02-02 15:40:45 -0600

    Floater Header 75% complete

---
+++ client/src/components/header/Navbar/MenuItems.jsx
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: 'Product',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'For creators',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Pricing',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Resources',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Log In',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Create on Patreon',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
