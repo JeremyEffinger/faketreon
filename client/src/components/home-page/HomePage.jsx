cbc8a5c353c41ffc281dbaf0815805ecf622bc43
cristian-kittsteiner <cdkittsteiner@yahoo.com>
2023-02-05 15:00:11 -0500

    place holders for carousel

--- client/src/components/home-page/HomePage.jsx
+++ client/src/components/home-page/HomePage.jsx
@@ -3,6 +3,17 @@ import { Link } from 'react-router-dom';
 import "./homePage.css";
 
 function HomePage (){
+
+    async function handleClick(){
+
+        const pathArray = window.location.pathname.split("/");
+        console.log(pathArray)
+
+    //     useEffect(() => {
+    //     setCurrentUrl(pathArray[1]);
+    //   }, []);
+    }
+
     return (
         <div>
             <div className="homePage">
@@ -10,6 +21,24 @@ function HomePage (){
                 <Link to="/posts">
                   <button>Posts</button>
                 </Link>
+
+                <div className="carousel">
+                    <div>
+                        <Link to="/trashtaste" onClick={handleClick}>
+                            TrashTaste
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to="/koboldpress">
+                            KoboldPress
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to="/something">
+                            Someone Else
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     )
