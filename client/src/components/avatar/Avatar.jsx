d4cfb51d737b82f803f71f8c924ee853749c3014
cristian-kittsteiner <cdkittsteiner@yahoo.com>
2023-02-05 21:01:15 -0500

    added link in footer, played with banner, not done

--- client/src/components/avatar/Avatar.jsx
+++ client/src/components/avatar/Avatar.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import style from "../avatar/avatar.module.scss";
 import { useRecoilValue } from "recoil";
 import CreatorInfoState from "../../CreatorInfoState";
+import "./avatar.css"
 
 const Avatar = (props) => {
   const text = useRecoilValue(CreatorInfoState);
 
   return (
     <div className={style.avatar}>
-      <img className={style.photo} src={text.user[0].avatar} />
+      <img className="photo" src={text.user[0].avatar} />
       <h4>{text.campaigns[0].title}</h4>
       <div>{text.campaigns[0].bio}</div>
     </div>
