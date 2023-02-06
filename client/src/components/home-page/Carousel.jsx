e2f3f2f32bd0bfca1bec716153baa3fbf52fe0f6
JeremyEffinger <Jeremy.Effinger@gmail.com>
2023-02-06 15:54:12 -0600

    fixed rendering.

--- client/src/components/home-page/Carousel.jsx
+++ client/src/components/home-page/Carousel.jsx
@@ -2,19 +2,11 @@ import React from "react";
 import { useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 
-
 export const Carousel = (props) => {
-    if(props.username === undefined) {
-        return <div></div>
-    }
-    return (
+  return (
     <div className="carousel">
-            <img className="photo" src={props.username[0].avatar} />
-            {console.log(props.username)}
-        </div>
-
-  )
-}
-
-
-
+      <img className="photo" src={props.username[0].avatar} />
+      {console.log(props.username)}
+    </div>
+  );
+};
--- client/src/components/home-page/HomePage.jsx
+++ client/src/components/home-page/HomePage.jsx
@@ -1,52 +1,57 @@
-import React from "react";
-import { useEffect } from "react";
-import { Link } from 'react-router-dom';
+import React, { Suspense } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./homePage.css";
+import Loading from "../../components/loading/Loading.jsx";
 import { useRecoilState, useRecoilValue } from "recoil";
 import UrlNameState from "../../UrlNameState.jsx";
 import CreatorInfoState from "../../CreatorInfoState.jsx";
 import axios from "axios";
 import { Carousel } from "./Carousel.jsx";
 
-function HomePage (props){
+function HomePage(props) {
+  const urlWithProxy = "/api/v1";
+  const [username, setUserName] = useState([]);
 
-    const urlWithProxy = "/api/v1";
-    const [username, setUserName] = ([]);
-  
-    useEffect(() => {
-      function getUsers() {
-        axios
-          .get(urlWithProxy + "/users")
-          .then((res) => {
-            console.log(res.data)
-            setUserName(res.data)})
-          .catch((err) => {
-            err;
-          });
-      }
-      getUsers();
-    }, [username]);
+  useEffect(() => {
+    function getUsers() {
+      axios
+        .get(urlWithProxy + "/users")
+        .then((res) => {
+          setUserName(res.data);
+        })
+        .catch((err) => {
+          err;
+        });
+    }
+    getUsers();
+  }, []);
 
+  if (username.length != 0) {
     return (
-    <div className="homePage">
-        <div className="header">
-            <h2 color="dark" className="Membership">Creativity powered by membership</h2>
-        </div>
-            <div>
-            <a className="Button" type="button" href="https://www.patreon.com/create">
-                <div className="getStarted">
-                    Get Started
-                </div>
+      <Suspense fallback={<Loading />}>
+        <div className="homePage">
+          <div className="header">
+            <h2 color="dark" className="Membership">
+              Creativity powered by membership
+            </h2>
+          </div>
+          <div>
+            <a
+              className="Button"
+              type="button"
+              href="https://www.patreon.com/create"
+            >
+              <div className="getStarted">Get Started</div>
             </a>
+          </div>
+          <Carousel username={username} />
         </div>
+      </Suspense>
+    );
+  } else {
+    return null;
+  }
+}
 
-        <Carousel username={username} />
-
-    </div>
-    )
-
-
-};
-
-export default HomePage
-
+export default HomePage;
